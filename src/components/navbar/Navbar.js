f75ebbef5a2db8e0e44a1ca8173a5974378e0756
Brian Kunkel <bkunkel412@yahoo.com>
2025-04-10 18:16:53 -0400

    Tweaks

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <div className="w-full px-3 h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
+    <div className="w-full px-2 lgl:px-4 h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
       <div>
         <img src={logo} alt="logo" />
       </div>
